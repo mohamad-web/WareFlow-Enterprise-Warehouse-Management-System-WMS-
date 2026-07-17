dfe6f051e4a91b2eaaac59725526f79a4deecac9
Mohamad <mhamadmm223344@gmail.com>
2026-07-17 22:16:31 +0100

    initial commit

---
+++ wareflow/src/main/java/com/wareflow/WareflowApplication.java
@@ -0,0 +1,13 @@
+package com.wareflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareflowApplication.class, args);
+	}
+
+}
